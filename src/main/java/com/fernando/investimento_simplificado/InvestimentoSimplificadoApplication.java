e630a4f39f17281b3f22fd923af22be2611b7802
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2024-12-09 22:50:08 -0300

    iniciando projeto api de investimento simplificado

---
+++ src/main/java/com/fernando/investimento_simplificado/InvestimentoSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.investimento_simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentoSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestimentoSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernando/investimento_simplificado/InvestimentoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.investimento_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentoSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
