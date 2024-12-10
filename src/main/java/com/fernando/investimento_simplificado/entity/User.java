c5d0aee8ea1300a02dcef29ce6838b62ebc75fb8
Fernando pereira <114356049+Fpereiraaraujo@users.noreply.github.com>
2024-12-09 23:08:34 -0300

    configurando a entidade

---
+++ src/main/java/com/fernando/investimento_simplificado/entity/User.java
@@ -0,0 +1,39 @@
+package com.fernando.investimento_simplificado.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    @UpdateTimestamp
+    private Instant UpdateTimeStamp;
+
+}
